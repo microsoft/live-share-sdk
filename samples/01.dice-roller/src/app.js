a229845c8104c21e84ed56a4c573ebc386afffa1
siduppal <suppal@microsoft.com>
2022-05-13 15:33:07 -0700

    Dice Roller Teams Tab App (#1)

--- samples/01.dice-roller/src/app.js
+++ samples/01.dice-roller/src/app.js
@@ -5,7 +5,7 @@
 
 import { SharedMap } from "fluid-framework";
 import { TeamsFluidClient } from "@microsoft/live-share";
-import { app } from "@microsoft/teams-js";
+import { app, pages, Context } from "@microsoft/teams-js";
 import { LOCAL_MODE_TENANT_ID } from "@fluidframework/azure-client";
 import { InsecureTokenProvider } from "@fluidframework/test-client-utils";
 
@@ -29,8 +29,24 @@ function onContainerFirstCreated(container) {
 // STARTUP LOGIC
 
 async function start() {
+
   // Check for page to display
-  const view = searchParams.get('view') || 'stage';
+  let view = searchParams.get('view') || 'stage';
+
+  // Check if we are running on stage.
+  if (!!searchParams.get('inTeams')) {
+
+    // Initialize teams app
+    await app.initialize();
+
+    // Get our frameContext from context of our app in Teams
+    const context = await app.getContext();
+    if (context.page.frameContext == 'meetingStage') {
+      view = 'stage';
+    }
+  }
+
+  // Load the requested view
   switch (view) {
     case 'content':
       renderSideBar(root);
@@ -50,9 +66,6 @@ async function joinContainer() {
   // Are we running in teams?
   let client;
   if (!!searchParams.get('inTeams')) {
-      // Initialize teams app
-      await app.initialize();
-
       // Create client
       client = new TeamsFluidClient();
   } else {
@@ -148,6 +161,21 @@ settingsTemplate["innerHTML"] = `
 
 function renderSettings(elem) {
     elem.appendChild(settingsTemplate.content.cloneNode(true));
+
+    // Save the configurable tab
+    pages.config.registerOnSaveHandler(saveEvent => {
+      pages.config.setConfig({
+        websiteUrl: window.location.origin,
+        contentUrl: window.location.origin + '?inTeams=1&view=content',
+        entityId: 'DiceRollerFluidLiveShare',
+        suggestedDisplayName: 'DiceRollerFluidLiveShare'
+      });
+      saveEvent.notifySuccess();
+    });
+
+    // Enable the Save button in config dialog
+    pages.config.setValidityState(true);
+
 }
 
 

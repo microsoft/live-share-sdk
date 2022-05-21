1d40d320f414165b0f209fb994e881f918bfcab1
Ryan Bliss <smile@ryanbliss.me>
2022-05-21 15:02:49 -0700

    updated video src

--- samples/21.react-media-template/src/utils/media-list.js
+++ samples/21.react-media-template/src/utils/media-list.js
@@ -9,7 +9,7 @@ export const mediaList = [
     thumbnailImage:
       "https://by3302files.storage.live.com/y4mohyKkLvuC6HfaeJbCtX1gnRdCHlLpJe1n6L4OLkd4z9ljbC_w-vcL0RJjs0k5IJvVY3AcJ1O0wmfG_Bwzs7CUCwqTTwuPNej7MiIbXqQDySexIq_qRUVK7v68bA4glUS6UBn-lmZBfiWI51h63-tZvhwtso6_55F1xjvo6z02v-K5yp56mTi0LsWPLblOPxL?width=660&height=371&cropmode=none",
     title: "Teams Channel Trailer",
-    src: "https://msdemomedia-uswe.streaming.media.azure.net/7e2b30c8-34d6-4ee6-bb43-b19f53c4c98a/15 - Channel Trailer V8.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
+    src: "https://livesharemedia-usw22.streaming.media.azure.net/d636cbe2-5e4a-419b-958e-6cc81dd3ca72/15 - Channel Trailer V8.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
     type: "video",
   },
 ];
@@ -19,17 +19,17 @@ export const searchList = [
   {
     id: "1",
     thumbnailImage:
-      "https://by3302files.storage.live.com/y4mD6C_s-s8NBbVQSdCKxRPbHRR2pCUgTOHu6BdtH_eWZr-jpubL8K1mfSKHFgOMH9Um8ZakHZGbnNFvLZV8mRwj06vFAe_Ac8j8EXURr-W3l4ngqF_s84YimrjL1guAZAefwENZ4krPSO4JJ7XDTf4QyWz-qm5zI-NTzfrI06NIwjqZowwFtUDyO8mRqPVNtK8?width=660&height=466&cropmode=none",
-    title: "Getting Started with Teams",
-    src: "https://msdemomedia-uswe.streaming.media.azure.net/13d9562d-9812-4202-873e-fd326e2dca31/Teams_YT_9_Get_Started_with_Team.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
+      "https://by3302files.storage.live.com/y4mJHPKYRDVa0Tq0tEL0MLkNp8Od4hPhcYudyjGdnU7DgErpEgEAflpu3f4QLCa5BjIwE11D2R3iKGauB3I0pbJDKSKrc6hMCB46pMB7fdsmbiAao0C5RmXOfzEZKcX6dFwQiIod2G0zeyfRL2akN9A8jw0Pf-sGWnKkXE-nMWv8-42_jM88n-F3PooGSSKaAyH?width=660&height=372&cropmode=none",
+    title: "Onboarding to Teams Apps",
+    src: "https://livesharemedia-usw22.streaming.media.azure.net/8bf16f5f-d4b0-437b-8121-d780abe744c9/Onboarding_Video11_Final_H264.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
     type: "video",
   },
   {
     id: "2",
     thumbnailImage:
-      "https://by3302files.storage.live.com/y4mtUo7UubnseilbKUGx7FmZI4WWR5AOGuk5zastkB6dB2ZU2xwWd7vz4l1Ee2W-O_fyLbqmdUFuXGsIPMxJke4NMBhdL-oNhZrbeTb9m9jHmmhC7ynqvQJYzw1TbuTKjC1A59NvPDES0m3ixNBhlUFmTk8VvPG_mZft30tDjtVUzrjYlo8BuNrAstwg-jpGkD-?width=1037&height=697&cropmode=none",
-    title: "Teams AMA",
-    src: "https://msdemomedia-uswe.streaming.media.azure.net/a4848b1e-35a4-43e6-8b9b-053dff499a8a/Teams_YT_4_AMA_Compilation_final.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
+      "https://by3302files.storage.live.com/y4mHWAobY6Eko5RrNOulkHfCymSqTxMZSIoivl3uOeNnteXJU2Gq_WY7TKfubdN0kJr04SThB11E_O4x0_sr-ic0v4yY3KE91R-Vv5gSYre76_xwKT7pc1KBl23yIgf3qgKaU2RFRWLF8WpEjoidwxxpSaqsSjhYXHFM26xqHGwUyxO8jobogFVw8RZ4GcIPYTl?width=660&height=374&cropmode=none",
+    title: "Beach 4K",
+    src: "https://livesharemedia-usw22.streaming.media.azure.net/39bd117d-0f75-40d2-8abe-fb7d49cf1157/pexels-ivan-khmelyuk-7222009.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
     type: "video",
   },
   {
@@ -37,7 +37,7 @@ export const searchList = [
     thumbnailImage:
       "https://by3302files.storage.live.com/y4mUBBvkvbh4U8GCw-auQJ4c0e9AoU9jk7OmqL1HoI5uv0ah2IVSngvGqhg4l-1P2w2rA1CqunF16uhBvYU_FXRpQq_UtsDg6cXovkskMxi-3tO-_r41HJg6Y2a_u0qkUl1amExyG7yl3Q8fmAvMRrXEg5D1ouyYX82YP9BqQDRFJlgVx9tCFk-jfLGVO1nMIru?width=660&height=369&cropmode=none",
     title: "3 app tips in Teams",
-    src: "https://msdemomedia-uswe.streaming.media.azure.net/f3218caa-9d45-4953-8c23-cdd17d3bcdaf/71372_teams_tip_three_apps_chat_.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
+    src: "https://livesharemedia-usw22.streaming.media.azure.net/113f04f1-5e06-45f5-be52-b848aa3f7afe/71372_teams_tip_three_apps_chat_.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
     type: "video",
   },
   {
@@ -45,7 +45,7 @@ export const searchList = [
     thumbnailImage:
       "https://by3302files.storage.live.com/y4mvA_fpovY-RkRbEAK3JFfyFJIJaikw2iFDNYeAPncjPRJ7jRfiJyVFiSk9Sq5qNXS7NpzBgBP9NHFr_I2KMs8qUgxkGTPebP86JQOHuezGclUtVPsmtzfA-7yYWJxFsRowzB6v6okwDQdC9il2Xq6183WBGgKWCwiCTX0HZ9ELVQFdUwfgxTK5BwEb79r_mPh?width=660&height=370&cropmode=none",
     title: "Reply to a message demo",
-    src: "https://msdemomedia-uswe.streaming.media.azure.net/fc09e019-56a8-4b98-82d8-ba40965b49bf/Reply_To_A_Message_Aisha_MASTER.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
+    src: "https://livesharemedia-usw22.streaming.media.azure.net/8961cf76-8e34-440f-b52d-e50601d8cf61/Reply_To_A_Message_Aisha_MASTER.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
     type: "video",
   },
   {
@@ -53,7 +53,7 @@ export const searchList = [
     thumbnailImage:
       "https://by3302files.storage.live.com/y4mL6V3p0Sqx7xCXX96BYtVxG-GSNNQ573Cc_SP769NWcpdjhhfOG9ZjzlndGQBqeoEkVARmtoW0rwyRe3Up7ldUin3YUhte0MSh5aYpxi68IDDJ19drmIDC6VF_h3w_2tPANAl27g15f4w42aANYhPM4VyeREVKp3ItAUNoULO6NXbQPooDXpfQMN4fYuNdqL6?width=660&height=374&cropmode=none",
     title: "Standout presenter demo",
-    src: "https://msdemomedia-uswe.streaming.media.azure.net/d762da1d-c59f-4e6a-8846-fc50e6fa0e40/Standout_Presenter_Aisha_MASTER.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
+    src: "https://livesharemedia-usw22.streaming.media.azure.net/a0e332ba-4b9c-4172-a7e4-f74e32eff698/Standout_Presenter_Aisha_MASTER.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
     type: "video",
   },
   {
@@ -61,23 +61,15 @@ export const searchList = [
     thumbnailImage:
       "https://by3302files.storage.live.com/y4m9tgZXn-22LyDf-IU2Wg_GMJE-JPMHbmO2fXO2DzI9ZGKYKguArpqcLTgmIOaIeaMDKoEDOOvYnIkHsys_Q7WvM8hHOGGJVwvHs1ksd12jiG8ZecJvevz_K7wE2VqqqG5Z3bZnDpuWvlolYmgzd_StHgYZodbmtHbNFrf5AUekdx8uNfDIptlP7Rqnt-mpO7P?width=1443&height=811&cropmode=none",
     title: "Hang Gliding",
-    src: "https://msdemomedia-uswe.streaming.media.azure.net/340f8319-e3b9-4208-8444-81056a3eba41/pexels-cédric-estienne-6749146.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
+    src: "https://livesharemedia-usw22.streaming.media.azure.net/2b941807-5fd5-45b6-bdc5-362c3c20ae8f/pexels-cédric-estienne-6749146.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
     type: "video",
   },
   {
     id: "7",
     thumbnailImage:
-      "https://by3302files.storage.live.com/y4mV9-EMTt1QDowalx-_xGPQnpGOVzz4aGX5-2JLVxJumMJwCoz9_sr0GUcrHB3czzMkb4B9CT5PXKqqcP6vggZEjyEmxkaxB5JwaRanenu1pe9U72gnWtL09GdIskIgE_Bk-wqMS0BQGjKrqMclQWnKZQp8k2q4IwYK2Nc5gQNjpJdY0b-K6B3saItzcCG6NGH?width=256&height=144&cropmode=none",
-    title: "Swiss Alps",
-    src: "https://msdemomedia-uswe.streaming.media.azure.net/6847b84d-2e82-40c5-b296-ffa6164f7d6c/production ID_3818213.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
-    type: "video",
-  },
-  {
-    id: "8",
-    thumbnailImage:
       "https://by3302files.storage.live.com/y4mzi99em6H2SBgawxkOet8pPzlPhQC2L4RWj8-qK3wZBhL_Ybol_8p04L_in28Phl7_D9hOMjl97aEjD93ZB7X18COjmHysfs0bK93goF_FMDBBEJUXAHbmkgvUHFj45VWtEN3n6BSoXZlvZWQXYoDRoGtuBLVMK7wbFUoYxE6ZJR0i8xvycB7bEQ2eJ2iSuVR?width=256&height=136&cropmode=none",
     title: "Dog Days",
-    src: "https://msdemomedia-uswe.streaming.media.azure.net/c739bb5a-8616-4d0f-b641-ceaed9d55dc0/pexels-zen-chung-5740700.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
+    src: "https://livesharemedia-usw22.streaming.media.azure.net/a5a174b6-3ca1-4b33-96fb-ce20b4b3eda1/pexels-zen-chung-5740700.ism/manifest(format=mpd-time-cmaf,encryption=cbc)",
     type: "video",
   },
 ];

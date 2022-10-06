db33713fe47f5c302478387adf8e0c960aab1e2b
David Claux <dclaux@users.noreply.github.com>
2022-10-05 17:34:04 -0700

    Get Live Canvas demo ready for Ignite (#97)
    * Add README to Live Canvas demo
    Remove simulation from demo
    Allow role selection in LiveCanvas
    
    * Default to stage view

--- packages/live-share-canvas/src/core/LiveCanvas.ts
+++ packages/live-share-canvas/src/core/LiveCanvas.ts
@@ -425,7 +425,7 @@ export class LiveCanvas extends DataObject {
         }
 
         // Setup incoming events
-        const scope = new EphemeralEventScope(this.runtime, [UserMeetingRole.presenter]);
+        const scope = new EphemeralEventScope(this.runtime, this.allowedRoles);
 
         this._pointerMovedEventTarget = new EphemeralEventTarget(
             scope,

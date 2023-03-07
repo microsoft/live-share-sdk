e1f0678503204ad43db29a7cef01cc525517fd69
Chengcheng Hu <chengchenghu@microsoft.com>
2023-03-06 16:39:50 -0800

    add bash script

---
+++ .azure-devops/scenario_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd samples/javascript/02.react-video
+echo "$PWD"
+npm run build
+npm run start & 
+success=0
+   tries=5
+   pause=1
+   while [ $success = 0 -a $tries -gt 0 ]; do
+     sleep $pause
+     let tries=tries-1
+     yarn test && success=1
+   done
+   if [ $success = 0 ]; then
+     echo "run failed"
+   fi

384db86f6f53e581d29aa86009353852b89c99af
Chengcheng Hu <chengchenghu@microsoft.com>
2023-03-07 10:25:36 -0800

    change pause

--- .azure-devops/scenario_test.sh
+++ .azure-devops/scenario_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 npm run start & 
 success=0
-   tries=5
-   pause=5
-   while [ $success = 0 -a $tries -gt 0 ]; do
-     sleep $pause
-     let tries=tries-1
-     yarn test > logs && success=1
-   done
-   if [ $success = 0 ]; then
-     echo "run failed"
-   fi
+tries=5
+pause=20
+while [ $success = 0 -a $tries -gt 0 ]; do
+    sleep $pause
+    let tries=tries-1
+    yarn test > logs && success=1
+done
+if [ $success = 0 ]; then
+    echo "run failed"
+fi

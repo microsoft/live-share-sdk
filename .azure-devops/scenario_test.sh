a4206e8f6b64ecf32ef0d8bb01f4dcd547798ee3
Chengcheng Hu <chengchenghu@microsoft.com>
2023-03-06 22:17:55 -0800

    fix

--- .azure-devops/scenario_test.sh
+++ .azure-devops/scenario_test.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-echo "$PWD"
-npm run build
 npm run start & 
 success=0
    tries=5
-   pause=1
+   pause=5
    while [ $success = 0 -a $tries -gt 0 ]; do
      sleep $pause
      let tries=tries-1
-     yarn test && success=1
+     yarn test > logs && success=1
    done
    if [ $success = 0 ]; then
      echo "run failed"

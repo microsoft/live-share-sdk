ea896d97324f906eb7f00f261359fb443eba98b3
Chengcheng Hu <chengchenghu@microsoft.com>
2023-03-07 14:37:12 -0800

    update bash

--- .azure-devops/scenario_test.sh
+++ .azure-devops/scenario_test.sh
@@ -2,11 +2,14 @@
 npm run start & 
 success=0
 tries=5
-pause=20
+pause=30
 while [ $success = 0 -a $tries -gt 0 ]; do
     sleep $pause
     let tries=tries-1
-    yarn test > logs && success=1
+    yarn test 1> logs 2> results &
+    ret = tail -f -n 1 errs | grep "Test Suites:"
+    echo $ret
+    success=1
 done
 if [ $success = 0 ]; then
     echo "run failed"

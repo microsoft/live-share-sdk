1b7ff3bd8144d76ee57815e834e6c2bc1a86846b
James Hunt <jameshunt@microsoft.com>
2023-05-19 15:29:56 -0500

    cleaned up build

---
+++ build-util.js
@@ -0,0 +1,11 @@
+/**
+ * Need because running npm workspaces command `npm run build -w="packages"` defined in package.json does not work in windows
+ */
+
+const directories = process.argv.slice(2);
+const optionsArray = directories.map((dir) => ["-w", dir]).flat();
+
+require("child_process").spawn("npm", ["run", "build"].concat(optionsArray), {
+    cwd: process.cwd(),
+    stdio: "inherit",
+});

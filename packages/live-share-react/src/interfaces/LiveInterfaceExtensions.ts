a0aab1db6d313f42d8e51dc80ef263a902915e34
Ryan Bliss <smile@ryanbliss.me>
2023-05-23 12:48:39 -0700

    updated IReceiveLiveEvent response (#606)

--- packages/live-share-react/src/interfaces/LiveInterfaceExtensions.ts
+++ packages/live-share-react/src/interfaces/LiveInterfaceExtensions.ts
@@ -3,7 +3,24 @@
  * Licensed under the Microsoft Live Share SDK License.
  */
 
+/**
+ * Object response for a received event
+ */
 export interface IReceiveLiveEvent<TEvent = any> {
+    /**
+     * The value of the event that was sent
+     */
     value: TEvent;
+    /**
+     * True if the local client was the one to send this event
+     */
     local: boolean;
+    /**
+     * Client ID of the user that sent the event
+     */
+    clientId: string;
+    /**
+     * Server timestamp at which the event was sent
+     */
+    timestamp: number;
 }

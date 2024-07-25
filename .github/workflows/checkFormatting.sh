d4d10b093240a90c204a2f90baaa4778344aecbf
James Hunt <jameshunt@microsoft.com>
2024-07-25 14:29:27 -0500

    cleanup

--- .github/workflows/checkFormatting.sh
+++ .github/workflows/checkFormatting.sh
@@ -4,11 +4,12 @@ npm run doctor
 diff=`git diff`
 if [ -z "$diff"  ]
 then
-echo "everything formatted"
-exit 0
-echo "" # empty line
-echo "" # empty line
-echo "FORMATTING NEEDED"
-echo "Please run 'npm run doctor'"
-exit 1
+    echo "everything formatted"
+    exit 0
+else
+    echo "" # empty line
+    echo "" # empty line
+    echo "FORMATTING NEEDED"
+    echo "Please run 'npm run doctor'"
+    exit 1
 fi

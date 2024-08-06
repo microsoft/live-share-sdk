85ef5c8448d5542aa2b387568a52a5171faa4fcb
James Hunt <jameshunt@microsoft.com>
2024-08-06 18:24:07 -0500

    apply suggestion from Jason Hartman, delete main index file, use the public and internal ones

--- packages/live-share/src/index.ts
+++
@@ -1,7 +0,0 @@
-/*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the Microsoft Live Share SDK License.
- */
-
-export * from "./index.public.js";
-export * from "./index.internal.js";

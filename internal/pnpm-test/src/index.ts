85df9429e7908dbb500c8ff6ccda91eecf293ec4
James Hunt <jameshunt@microsoft.com>
2024-08-08 18:37:17 -0500

    ensure pnpm is supported and add a CI check

---
+++ internal/pnpm-test/src/index.ts
@@ -0,0 +1,22 @@
+import { LiveShareClient, TestLiveShareHost } from "@microsoft/live-share";
+import { waitForDelay } from "@microsoft/live-share/internal";
+
+testImportPackages();
+
+async function testImportPackages() {
+    const canUseLiveShare =
+        new LiveShareClient(TestLiveShareHost.create()) !== undefined;
+    const canUseLiveShareInternal = waitForDelay(10) !== undefined;
+
+    if (!canUseLiveShare) {
+        process.exit(1);
+    }
+
+    if (!canUseLiveShareInternal) {
+        process.exit(2);
+    }
+
+    console.log(
+        "Sucessfully imported and able to use Live Share package in pnpm project"
+    );
+}
---
+++ internal/pnpm-test/test.sh
@@ -0,0 +1,4 @@
+pnpm install &&
+pnpm run build &&
+node bin/index.js &&
+echo "Live Share package was successfully imported and used by app using pnpm"

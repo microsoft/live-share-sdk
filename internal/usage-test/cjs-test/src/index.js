180f172334835db6d2648480af918e96b319adce
James Hunt <jameshunt@microsoft.com>
2024-08-09 12:00:41 -0500

    fixed path

--- internal/usage-test/cjs-test/src/index.js
+++ internal/usage-test/cjs-test/src/index.js
@@ -1,8 +1,8 @@
 const { LiveShareClient, TestLiveShareHost } = require("@microsoft/live-share");
 const { waitForDelay } = require("@microsoft/live-share/internal");
 
-const canUseLiveShare =
-    new LiveShareClient(TestLiveShareHost.create()) !== undefined;
+const client = new LiveShareClient(TestLiveShareHost.create());
+const canUseLiveShare = client !== undefined;
 const canUseLiveShareInternal = waitForDelay(10) !== undefined;
 
 if (!canUseLiveShare) {
@@ -13,6 +13,7 @@ if (!canUseLiveShareInternal) {
     process.exit(2);
 }
 
+console.log("client", client);
 console.log(
     "Sucessfully imported and able to use Live Share package in cjs project"
 );
--- internal/usage-test/esm-test/src/index.js
+++ internal/usage-test/esm-test/src/index.js
@@ -1,8 +1,8 @@
 import { LiveShareClient, TestLiveShareHost } from "@microsoft/live-share";
 import { waitForDelay } from "@microsoft/live-share/internal";
 
-const canUseLiveShare =
-    new LiveShareClient(TestLiveShareHost.create()) !== undefined;
+const client = new LiveShareClient(TestLiveShareHost.create());
+const canUseLiveShare = client !== undefined;
 const canUseLiveShareInternal = waitForDelay(10) !== undefined;
 
 if (!canUseLiveShare) {
@@ -13,6 +13,7 @@ if (!canUseLiveShareInternal) {
     process.exit(2);
 }
 
+console.log("client", client);
 console.log(
     "Sucessfully imported and able to use Live Share package in esm project"
 );

9620b26972093c36d46820d6059eabf396021d5e
Ryan Bliss <smile@ryanbliss.me>
2025-09-04 13:30:11 -0700

    Fixed unit tests for media

--- packages/live-share-media/src/test/LiveMediaSession_ManualActionHandler.spec.ts
+++ packages/live-share-media/src/test/LiveMediaSession_ManualActionHandler.spec.ts
@@ -15,6 +15,7 @@ import {
     LocalTimestampProvider,
     UserMeetingRole,
     TestLiveShareHost,
+    LiveShareClient,
 } from "@microsoft/live-share";
 import {
     getLiveDataObjectKind,
@@ -35,160 +36,255 @@ import {
     ITestObjectProviderOptions,
     describeCompat,
 } from "@live-share-private/test-utils";
+import { InsecureTokenProvider } from "@fluidframework/test-runtime-utils/internal";
+import { ContainerSchema, IFluidContainer } from "fluid-framework";
+import { AzureContainerServices } from "@fluidframework/azure-client";
+import { LiveMediaSession } from "../LiveMediaSession";
 
-async function getObjects(
-    getTestObjectProvider: (
-        options?: ITestObjectProviderOptions
-    ) => ITestObjectProvider,
-    updateInterval: number = 10000,
-    timestampProvider: ITimestampProvider = new LocalTimestampProvider()
-) {
-    const host = TestLiveShareHost.create(new MockTokenProvider());
-    let liveRuntime1 = new MockLiveShareRuntime(
-        false,
-        updateInterval,
-        host,
-        timestampProvider
-    );
-    let liveRuntime2 = new MockLiveShareRuntime(
-        false,
-        updateInterval,
-        host,
-        timestampProvider
-    );
+// async function getObjects(
+//     getTestObjectProvider: (
+//         options?: ITestObjectProviderOptions
+//     ) => ITestObjectProvider,
+//     updateInterval: number = 10000,
+//     timestampProvider: ITimestampProvider = new LocalTimestampProvider()
+// ) {
+//     const host = TestLiveShareHost.create(new MockTokenProvider());
+//     let liveRuntime1 = new MockLiveShareRuntime(
+//         false,
+//         updateInterval,
+//         host,
+//         timestampProvider
+//     );
+//     let liveRuntime2 = new MockLiveShareRuntime(
+//         false,
+//         updateInterval,
+//         host,
+//         timestampProvider
+//     );
 
-    let ObjectProxy1: any = getLiveDataObjectKind<TestLiveMediaSession>(
-        TestLiveMediaSession,
-        liveRuntime1 as unknown as LiveShareRuntime
-    );
-    let ObjectProxy2: any = getLiveDataObjectKind<TestLiveMediaSession>(
-        TestLiveMediaSession,
-        liveRuntime2 as unknown as LiveShareRuntime
-    );
+//     let ObjectProxy1: any = getLiveDataObjectKind<TestLiveMediaSession>(
+//         TestLiveMediaSession,
+//         liveRuntime1 as unknown as LiveShareRuntime
+//     );
+//     let ObjectProxy2: any = getLiveDataObjectKind<TestLiveMediaSession>(
+//         TestLiveMediaSession,
+//         liveRuntime2 as unknown as LiveShareRuntime
+//     );
 
-    await liveRuntime1.start();
-    await liveRuntime2.start();
+//     await liveRuntime1.start();
+//     await liveRuntime2.start();
 
-    let provider: ITestObjectProvider = getTestObjectProvider();
+//     let provider: ITestObjectProvider = getTestObjectProvider();
 
-    let container1 = await provider.createContainer(
-        ObjectProxy1.factory as fluidEntryPoint
-    );
-    let object1 =
-        await getContainerEntryPointBackCompat<TestLiveMediaSession>(
-            container1
-        );
-    object1.coordinator.positionUpdateInterval = 0.02;
-    let container2 = await provider.loadContainer(
-        ObjectProxy2.factory as fluidEntryPoint
-    );
-    let object2 =
-        await getContainerEntryPointBackCompat<TestLiveMediaSession>(
-            container2
-        );
-    object2.coordinator.positionUpdateInterval = 0.02;
+//     let container1 = await provider.createContainer(
+//         ObjectProxy1.factory as fluidEntryPoint
+//     );
+//     let object1 =
+//         await getContainerEntryPointBackCompat<TestLiveMediaSession>(
+//             container1
+//         );
+//     object1.coordinator.positionUpdateInterval = 0.02;
+//     let container2 = await provider.loadContainer(
+//         ObjectProxy2.factory as fluidEntryPoint
+//     );
+//     let object2 =
+//         await getContainerEntryPointBackCompat<TestLiveMediaSession>(
+//             container2
+//         );
+//     object2.coordinator.positionUpdateInterval = 0.02;
 
-    const track1 = {
-        trackIdentifier: "track1",
-        title: "Test Track 1",
-    } as ExtendedMediaMetadata;
+//     const track1 = {
+//         trackIdentifier: "track1",
+//         title: "Test Track 1",
+//     } as ExtendedMediaMetadata;
 
-    const getMediaPlayerState: () => IMediaPlayerState = () => {
-        return {
-            metadata: track1,
-            playbackState: "none",
-            positionState: undefined,
-            trackData: null,
-        };
-    };
+//     const getMediaPlayerState: () => IMediaPlayerState = () => {
+//         return {
+//             metadata: track1,
+//             playbackState: "none",
+//             positionState: undefined,
+//             trackData: null,
+//         };
+//     };
 
-    object1.setRequestPlayerStateHandler(getMediaPlayerState);
-    object2.setRequestPlayerStateHandler(getMediaPlayerState);
+//     object1.setRequestPlayerStateHandler(getMediaPlayerState);
+//     object2.setRequestPlayerStateHandler(getMediaPlayerState);
 
-    // need to be connected to send signals
-    if (!container1.connect) {
-        await new Promise((resolve) => container1.once("connected", resolve));
-    }
-    if (!container2.connect) {
-        await new Promise((resolve) => container2.once("connected", resolve));
-    }
-    const setObjectRoles = async (
-        clientId: string,
-        roles: UserMeetingRole[]
-    ) => {
-        (host as TestLiveShareHost).addClient(clientId, roles);
-    };
-    const dispose = () => {
-        object1.dispose();
-        object2.dispose();
-        container1.disconnect?.();
-        container2.disconnect?.();
-        liveRuntime1.stop();
-        liveRuntime2.stop();
-    };
-    return {
-        object1,
-        object2,
-        dispose,
-        setObjectRoles,
-    };
-}
+//     // need to be connected to send signals
+//     if (!container1.connect) {
+//         await new Promise((resolve) => container1.once("connected", resolve));
+//     }
+//     if (!container2.connect) {
+//         await new Promise((resolve) => container2.once("connected", resolve));
+//     }
+//     const setObjectRoles = async (
+//         clientId: string,
+//         roles: UserMeetingRole[]
+//     ) => {
+//         (host as TestLiveShareHost).addClient(clientId, roles);
+//     };
+//     const dispose = () => {
+//         object1.dispose();
+//         object2.dispose();
+//         container1.disconnect?.();
+//         container2.disconnect?.();
+//         liveRuntime1.stop();
+//         liveRuntime2.stop();
+//     };
+//     return {
+//         object1,
+//         object2,
+//         dispose,
+//         setObjectRoles,
+//     };
+// }
 
 describeCompat(
     "LiveMediaSession Manual Action Handlers (mostly testing coordinator)",
-    (getTestObjectProvider) => {
-        it("should send 'positionUpdate' event when someone joins.", async () => {
-            const { object1, object2, dispose } = await getObjects(
-                getTestObjectProvider
-            );
+    () => {
+        let containerId: string | undefined;
+        const getContainerId = (): string | undefined => {
+            return containerId;
+        };
+        const setContainerId = (newContainerId: string) => {
+            containerId = newContainerId;
+        };
+        const host = TestLiveShareHost.create(
+            new InsecureTokenProvider("", {
+                id: "testId",
+                name: "Test User",
+            }),
+            getContainerId,
+            setContainerId
+        );
+        let client1: LiveShareClient;
+        let client2: LiveShareClient;
+        let object1: LiveMediaSession;
+        let object2: LiveMediaSession;
 
+        const testMediaSessionKey = "TEST-LIVE-MEDIA-SESSION-KEY";
+
+        const dispose = () => {
+            object1.dispose();
+            object2.dispose();
+            client1.results?.container?.disconnect?.();
+            client2.results?.container?.disconnect?.();
+        };
+
+        const setObjectRoles = async (
+            clientId: string,
+            roles: UserMeetingRole[]
+        ) => {
+            (host as TestLiveShareHost).addClient(clientId, roles);
+        };
+
+        const start = async (
+            timestampProvider: ITimestampProvider = new LocalTimestampProvider()
+        ) => {
+            client1 = new LiveShareClient(host, {
+                timestampProvider,
+            });
+            client2 = new LiveShareClient(host, {
+                timestampProvider,
+            });
+            const schema: ContainerSchema = {
+                initialObjects: {
+                    [testMediaSessionKey]: LiveMediaSession,
+                },
+            };
+            containerId = undefined;
+
+            await client1.join(schema);
+            await client2.join(schema);
+            // state map not initialized until dynamic features are used.
+            object1 = await client1.getDDS<LiveMediaSession>(
+                "test",
+                LiveMediaSession
+            );
+            object1.coordinator.positionUpdateInterval = 0.02;
+            object2 = await client2.getDDS<LiveMediaSession>(
+                "test",
+                LiveMediaSession
+            );
+            object2.coordinator.positionUpdateInterval = 0.02;
+
+            const track1 = {
+                trackIdentifier: "track1",
+                title: "Test Track 1",
+            } as ExtendedMediaMetadata;
+
+            const getMediaPlayerState: () => IMediaPlayerState = () => {
+                return {
+                    metadata: track1,
+                    playbackState: "none",
+                    positionState: undefined,
+                    trackData: null,
+                };
+            };
+
+            object1.setRequestPlayerStateHandler(getMediaPlayerState);
+            object2.setRequestPlayerStateHandler(getMediaPlayerState);
+        };
+
+        it("should send 'positionUpdate' event when someone joins.", async () => {
+            await start();
             await object1.initialize();
             // wait for next event loop, simulate existing user waiting for other people to join.
             // otherwise joined event will fire for both users
             await waitForDelay(1);
             let positionUpdateCount = 0;
 
-            await object2.initialize();
-            // @ts-ignore-next-line
-            object2.coordinator._positionUpdateEvent!._scope.onEvent(
-                "positionUpdate",
-                (event, local) => {
-                    // assert(!local, JSON.stringify(event));
-                    positionUpdateCount += 1;
-                }
+            const scope2 = new LiveEventScope(
+                // @ts-ignore-next-line
+                object2.runtime,
+                // @ts-ignore-next-line
+                object2._liveRuntime
             );
+
+            scope2.onEvent("positionUpdate", (event, local) => {
+                // assert(!local, JSON.stringify(event));
+                positionUpdateCount += 1;
+            });
+
+            await object2.initialize();
+
             // wait for next event loop
             await waitForDelay(1);
 
-            // expected 2
-            // case 1: object2 joins, listens for itself, and sends a position update
+            // expected 1
+            // case 1: object2 joins, listens for itself (no event is emitted for local updates), and sends a position update
+            //  - reason no local event is emitted is because the targetClientId for position updates is for the remote user
             // case 2: object2 joins, object1 listens for it, and sends a position update
             assert(
-                positionUpdateCount == 2,
+                positionUpdateCount == 1,
                 `positionUpdate event not sent, ${positionUpdateCount}`
             );
+
             dispose();
         });
 
         it("should not send 'positionUpdate' event if object has canSendPositionUpdates set to false", async () => {
-            const { object1, object2, dispose } = await getObjects(
-                getTestObjectProvider
-            );
-
+            await start();
             await object1.initialize();
             // wait for next event loop, simulate existing user waiting for other people to join.
             // otherwise joined event will fire for both users
             await waitForDelay(1);
 
-            // create a duplicate scope/target with same event name as one declared in coordinator
-            const scope2 = new LiveEventScope(
-                object2.runtimeForTesting(),
-                object2.liveRuntimeForTesting()
-            );
             let positionUpdateCount = 0;
-            new LiveEventTarget(scope2, "positionUpdate", (event, local) => {
-                positionUpdateCount += 1;
-            });
+            const scope2 = new LiveEventScope(
+                // @ts-ignore-next-line
+                object2.runtime,
+                // @ts-ignore-next-line
+                object2._liveRuntime
+            );
+            new LiveEventTarget(
+                // @ts-ignore-next-line
+                scope2,
+                "positionUpdate",
+                (event, local) => {
+                    positionUpdateCount += 1;
+                }
+            );
 
             object2.coordinator.canSendPositionUpdates = false;
             await object2.initialize();
@@ -201,13 +297,12 @@ describeCompat(
                 positionUpdateCount == 1,
                 `positionUpdate event not sent, ${positionUpdateCount}`
             );
+
             dispose();
         });
 
         it("should send 'positionUpdate' event if object has canSendPositionUpdates set to false, but is suspended", async () => {
-            const { object1, object2, dispose, setObjectRoles } =
-                await getObjects(getTestObjectProvider);
-
+            await start();
             const positionState: (position: number) => IMediaPlayerState = (
                 position: number
             ) => ({
@@ -219,12 +314,16 @@ describeCompat(
                 trackData: null,
             });
 
-            setObjectRoles(await object1.clientId(), [
-                UserMeetingRole.organizer,
-            ]);
-            setObjectRoles(await object2.clientId(), [
-                UserMeetingRole.attendee,
-            ]);
+            setObjectRoles(
+                // @ts-ignore-next-line
+                await object1.waitUntilConnected(),
+                [UserMeetingRole.organizer]
+            );
+            setObjectRoles(
+                // @ts-ignore-next-line
+                await object2.waitUntilConnected(),
+                [UserMeetingRole.attendee]
+            );
 
             await object1.initialize([
                 UserMeetingRole.presenter,
@@ -259,14 +358,12 @@ describeCompat(
                 (object2.coordinator as any)._groupState?.playbackPosition
                     .targetPosition === 30
             );
+
             dispose();
         });
 
         it("should send 'positionUpdate' event regularly.", async () => {
-            const { object1, object2, dispose } = await getObjects(
-                getTestObjectProvider
-            );
-
+            await start();
             let startOfTest = Date.now();
             const positionState: () => IMediaPlayerState = () => ({
                 metadata: null,
@@ -276,23 +373,27 @@ describeCompat(
                 },
                 trackData: null,
             });
-
-            // create a duplicate scope/target with same event name as one declared in coordinator
-            const scope1 = new LiveEventScope(
-                object1.runtimeForTesting(),
-                object1.liveRuntimeForTesting()
-            );
-
             let posUpdateCount = 0;
             const done = new Deferred();
-            new LiveEventTarget(scope1, "positionUpdate", (event, local) => {
-                if (!local) return;
+            const scope1 = new LiveEventScope(
+                // @ts-ignore-next-line
+                object1.runtime,
+                // @ts-ignore-next-line
+                object1._liveRuntime
+            );
+            new LiveEventTarget(
+                // @ts-ignore-next-line
+                scope1,
+                "positionUpdate",
+                (event, local) => {
+                    if (!local) return;
 
-                posUpdateCount += 1;
-                if (posUpdateCount > 6) {
-                    done.resolve();
+                    posUpdateCount += 1;
+                    if (posUpdateCount > 6) {
+                        done.resolve();
+                    }
                 }
-            });
+            );
 
             await object1.initialize();
             await object2.initialize();
@@ -313,10 +414,7 @@ describeCompat(
         });
 
         it("should broadcast 'play' transport command.", async () => {
-            const { object1, object2, dispose } = await getObjects(
-                getTestObjectProvider
-            );
-
+            await start();
             const donePlay1 = new Deferred();
             const donePlay2 = new Deferred();
 
@@ -340,13 +438,12 @@ describeCompat(
             await donePlay1.promise;
             await donePlay2.promise;
             await waitForDelay(1);
+
             dispose();
         });
 
         it("should broadcast 'pause' transport command.", async () => {
-            const { object1, object2, dispose } = await getObjects(
-                getTestObjectProvider
-            );
+            await start();
 
             const donePause1 = new Deferred();
             const donePause2 = new Deferred();
@@ -371,14 +468,12 @@ describeCompat(
             await donePause1.promise;
             await donePause2.promise;
             await waitForDelay(1);
+
             dispose();
         });
 
         it("should broadcast 'seekTo' transport command.", async () => {
-            const { object1, object2, dispose } = await getObjects(
-                getTestObjectProvider
-            );
-
+            await start();
             const done1 = new Deferred();
             const done2 = new Deferred();
 
@@ -404,14 +499,12 @@ describeCompat(
             await done1.promise;
             await done2.promise;
             await waitForDelay(1);
+
             dispose();
         });
 
         it("should broadcast 'setTrack' transport command.", async () => {
-            const { object1, object2, dispose } = await getObjects(
-                getTestObjectProvider
-            );
-
+            await start();
             const done1 = new Deferred();
             const done2 = new Deferred();
 
@@ -448,15 +541,12 @@ describeCompat(
             await done1.promise;
             await done2.promise;
             await waitForDelay(1);
+
             dispose();
         });
 
         it("should broadcast 'catchup' transport command.", async () => {
-            const { object1, object2, dispose } = await getObjects(
-                getTestObjectProvider,
-                10000,
-                new TestMediaTimeStampProvider()
-            );
+            await start(new TestMediaTimeStampProvider());
 
             const done = new Deferred();
 
@@ -506,6 +596,7 @@ describeCompat(
 
             await object1.coordinator.play();
             await done.promise;
+
             dispose();
         });
     }

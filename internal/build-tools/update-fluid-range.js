e504cf03e6a0c21cd12049b4ecc4a8adec890469
James Hunt <jameshunt@microsoft.com>
2026-04-27 12:56:03 -0500

    update fluid versions to >=2.40 <2.110 and live share versions to 2.0.0-internal.17

--- internal/build-tools/update-fluid-range.js
+++ internal/build-tools/update-fluid-range.js
@@ -12,10 +12,10 @@ const fs = require("fs");
 const path = require("path");
 const { getRootFolder } = require("./utils");
 
-const DEFAULT_FROM_RANGE = ">=2.40 <2.90";
-const DEFAULT_TO_RANGE = ">=2.40 <2.100";
-const DEFAULT_VERSION_FROM = "2.0.0-internal.15";
-const DEFAULT_VERSION_TO = "2.0.0-internal.16";
+const DEFAULT_FROM_RANGE = ">=2.40 <2.100";
+const DEFAULT_TO_RANGE = ">=2.40 <2.110";
+const DEFAULT_VERSION_FROM = "2.0.0-internal.16";
+const DEFAULT_VERSION_TO = "2.0.0-internal.17";
 const INTERNAL_PKG_PREFIX = "@microsoft/live-share";
 const IGNORED_FOLDERS = new Set([
     "node_modules",
